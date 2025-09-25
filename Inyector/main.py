06dbb960f058ba6c1c0419522eb0f3c178523594
Areo-17 <arielbg6@hotmail.com>
2025-09-25 10:07:47 -0600

    Modified main.py. Modified Dockerfile.

--- Inyector/main.py
+++ Inyector/main.py
@@ -7,8 +7,6 @@ load_dotenv()
 URL_API = os.getenv('URL_API')
 with open('./data/content.json','r') as file: content_json = json.load(file)
 content = content_json.items()
-users_df = pd.read_csv('./data/users.csv')
-viewing_df = pd.read_csv('./data/viewing.csv')
 
 # ---------------- PROCESAMIENTO DE DOCUMENTOS --------------------- # 
 
@@ -42,7 +40,7 @@ def transform_csv(file_path: str, users: bool = True):
     Converts the CSV format into JSON-like to send it as an API payload. The function creates a list with JSON items, each item has keys based on the CSV column names and each key has a single value from the data related to their respective column.
     ## **Parameters**
     - ```file_path```: Path to the file that will be processed.
-    - ```users```: Determine whether the file is for the table users. Otherwise, the table will be viewing_sessions.
+    - ```users```: Determine whether the file is for the table "users", otherwise, the table will be "viewing_sessions".
 
     ## **Returns**
     A dictionary with the table name and its data:\n
@@ -68,7 +66,7 @@ def transform_csv(file_path: str, users: bool = True):
         table_name = 'viewing_sessions'
     df.to_json(path_or_buf=f"./data/{table_name}_processed.json", orient='records')
     #Giving a few seconds to process and save the file.
-    time.sleep(3)
+    time.sleep(6)
     with open(f"./data/{table_name}_processed.json", "r") as json_raw:
         content_processed = json.load(json_raw)
     payload = requests.post(
@@ -80,7 +78,7 @@ def transform_csv(file_path: str, users: bool = True):
     return payload
 
 if __name__ == '__main__':
-    json_processed(id_prefix="M")
-    json_processed(id_prefix="S")
-    transform_csv('./data/users.csv', True)
-    transform_csv('./data/viewing_sessions.csv', False)
+    json_processed(id_prefix="M") # Creates collection "movies" and inserts data
+    json_processed(id_prefix="S") # Creates collection "series" and inserts data
+    transform_csv('./data/users.csv', True) # Creates table "users" and inserts data
+    transform_csv('./data/viewing_sessions.csv', False) # Creates table "viewing_sessions" and inserts data
